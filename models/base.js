55d5fe27286472fa40f9553e7bf70147d0f32860
Jankid <gao464303875@126.com>
2017-08-03 16:34:03 +0800

    修改db

--- models/base.js
+++ models/base.js
@@ -2,10 +2,9 @@ var mongoose = require('mongoose');
 var sqlServer = require('../server.config').sqlServer
 var Schema = mongoose.Schema;
 var models = require("./index");
-
 mongoose.Promise = global.Promise
 mongoose.connect(`mongodb://${sqlServer.host}:${sqlServer.port}/${sqlServer.database}`,{ useMongoClient:true })
-
+const db = mongoose.connection;
 for(var m in models){ 
     mongoose.model(m,new Schema(models[m]));
 }
@@ -13,6 +12,22 @@ for(var m in models){
 var _getModel = function(type){ 
     return mongoose.model(type);
 };
+
+
+
+db.once('open' ,() => {
+	console.log('连接数据成功')
+})
+
+db.on('error', function(error) {
+    console.error('Error in MongoDb connection: ' + error);
+    mongoose.disconnect();
+});
+
+db.on('close', function() {
+    console.log('数据库断开，重新连接数据库');
+    mongoose.connect(`mongodb://${sqlServer.host}:${sqlServer.port}/${sqlServer.database}`,{ useMongoClient:true });
+});
 
 module.exports = { 
     getModel: function(type){ 
